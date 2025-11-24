4a1cad70430c030451a8e060797ceed747e6f5a4
ravindu644 <droidcasts@protonmail.com>
2025-11-24 15:59:22 +0530

    CRITICAL: kernel: cmdline: fix firmware class path
    Signed-off-by: ravindu644 <droidcasts@protonmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -67,7 +67,7 @@ TW_CUSTOM_BATTERY_POS := "750"
 BOARD_KERNEL_CMDLINE :=  \
 	video=vfb:640x400,bpp=32,memsize=3072000 \
 	printk.devkmsg=on \
-	firmware_class.path=/vendor/firmware_mnt/image \
+	firmware_class.path=/vendor/firmware,/vendor/firmware_mnt/image,/efs/wifi \
 	onsole=null \
 	bootconfig \
 	androidboot.hardware=qcom \
@@ -83,7 +83,7 @@ BOARD_KERNEL_CMDLINE :=  \
 # Kernel
 BOARD_BOOTIMG_HEADER_VERSION := 2
 BOARD_KERNEL_BASE := 0x00000000
-BOARD_KERNEL_CMDLINE := video=vfb:640x400,bpp=32,memsize=3072000 firmware_class.path=/vendor/firmware_mnt/image printk.devkmsg=on bootconfig androidboot.hardware=qcom androidboot.memcg=1 androidboot.load_modules_parallel=true androidboot.usbcontroller=4e00000.dwc3 loop.max_part=7 hx83112f.tpsensor=HIMAX_TXD_HX83112F ilitek.tpsensor=ILITEK_TMB_ILI7807S
+BOARD_KERNEL_CMDLINE := video=vfb:640x400,bpp=32,memsize=3072000 firmware_class.path=/vendor/firmware,/vendor/firmware_mnt/image,/efs/wifi printk.devkmsg=on bootconfig androidboot.hardware=qcom androidboot.memcg=1 androidboot.load_modules_parallel=true androidboot.usbcontroller=4e00000.dwc3 loop.max_part=7 hx83112f.tpsensor=HIMAX_TXD_HX83112F ilitek.tpsensor=ILITEK_TMB_ILI7807S
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_RAMDISK_OFFSET := 0x02000000
 BOARD_KERNEL_TAGS_OFFSET := 0x01e00000
